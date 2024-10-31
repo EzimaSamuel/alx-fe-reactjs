76c84c70482e1682482fa467142fd5cf1dcf2e53
Ezima Samuel <samuelunn@gmail.com>
2024-10-31 02:32:36 -0700

    UserProfile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import WelcomeMessage from './src/components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -15,6 +16,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
       <WelcomeMessage />
 
