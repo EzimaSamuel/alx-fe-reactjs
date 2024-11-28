4ac4dde56470a0231c62a2daa4832d2130dcae19
Ezima Samuel <samuelunn@gmail.com>
2024-11-28 00:44:14 -0800

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
           alt="User"
           className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
         />
-        <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center">
+        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center">
           John Doe
         </h1>
         <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center">
