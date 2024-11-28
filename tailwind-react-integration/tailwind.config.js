4ce6ecd97227199b0b83015eb1accb7a7bdebb92
Ezima Samuel <samuelunn@gmail.com>
2024-11-27 23:57:43 -0800

    tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // 'media' or 'class' for dark mode configuration
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
